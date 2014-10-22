cedefb1a2ce3dac78d375e130aca435cd567ce74
Jade Meskill and Roy van de Water <dev+jade+roy@integrumtech.com>
2014-10-21 18:13:16 -0700

    Copy the ethernet stuff to wifi

--- examples/microblu_wifi/microblu_wifi.ino
+++ examples/microblu_wifi/microblu_wifi.ino
@@ -1,9 +1,13 @@
-#include <PubSubClient.h>
-#include <Microblu.h>
 #include <SPI.h>
 #include <WiFi.h>
+#include <Servo.h>
+#include <Wire.h>
+#include <Firmata.h>
+#include <Microblu.h>
+#include <StdFirmata.h>
+#include <PubSubClient.h>
 
-#define MESHBLU_HOST "mqtt.octoblu.com"
+#define MESHBLU_HOST "192.168.200.43"
 #define MESHBLU_PORT 1883
 #define UUID  "68ce70e1-5636-11e4-a414-671a5d380d7e"
 #define TOKEN "661sg1wxr61ug14ifgtnggr1xwfry66r"
@@ -11,35 +15,31 @@
 #define WIFI_SSID       "OB-HQ"
 #define WIFI_PASSWORD   "0ct0b1u2014"
 
-Microblu microblu(UUID, TOKEN, MESHBLU_HOST, MESHBLU_PORT);
+Microblu *microblu  = new Microblu(UUID, TOKEN, MESHBLU_HOST, MESHBLU_PORT);
+StdFirmata *firmata = new StdFirmata();
 WiFiClient networkClient;
 
 int status = WL_IDLE_STATUS;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("SYSTEM REBOOT");
 
-  // check for the presence of the shield:
   if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("WiFi shield not present");
-    // don't continue:
     while(true);
   }
 
-  // attempt to connect to Wifi network:
   while ( status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(WIFI_SSID);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-    // wait 10 seconds for connection:
     delay(10000);
   }
 
-  microblu.initialize(networkClient);
+  microblu->initialize(networkClient, firmata);
 }
 
 void loop() {
-  microblu.loop();
+  microblu->loop();
 }
